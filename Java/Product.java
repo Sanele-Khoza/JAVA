50dd1c6d9a4a1a39d307b26bc9c1b4d150cde5be
Sanele-Khoza <sanelerondo012@gmail.com>
2025-02-26 14:12:57 +0200

    Create Product.java

---
+++ Java/Product.java
@@ -0,0 +1,30 @@
+import java.util.Scanner; // Import the Scanner class
+
+public class Product2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Create Scanner object to allow inputs from the keyboard
+
+        String Product;
+        int quantity;
+        double price;
+        double total;
+
+        System.out.println("What would you want: ");
+        Product = scanner.nextLine();
+        System.out.println("How much is it: ");
+        price = scanner.nextDouble();
+        System.out.println("How many do you want: ");
+        quantity = scanner.nextInt();
+
+        System.out.println("Your Order is: ");
+        System.out.println("-----------------");
+        System.out.println("Product: " + Product);
+        System.out.println("Price: " + price);
+        System.out.println("Quantity: " + quantity);
+
+        total = quantity*price;
+
+        System.out.println("Your total order is: " +"R" + total);
+
+    }
+}
