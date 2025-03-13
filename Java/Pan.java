b66e0d7fc989f10d17a34b2149bf31b71b57c622
Sanele-Khoza <sanelerondo012@gmail.com>
2025-03-13 12:34:08 +0200

    Create Pan.java

---
+++ Java/Pan.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class pan {
+    public static void main(String[] args) {
+        displayResult("Test message from pan.java");
+    }
+
+    public static void displayResult(String message) {
+        JOptionPane.showMessageDialog(null, message);
+    }
+}
